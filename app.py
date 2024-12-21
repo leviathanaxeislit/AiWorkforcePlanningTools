0e0e5d83fbf6dbdb682a3a290b5dc7e67850a190
leviathanaxeislit <scp.thedevil@gmail.com>
2024-12-21 18:28:11 +0530

    styling
    Signed-off-by: leviathanaxeislit <scp.thedevil@gmail.com>

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 
+with open( "styles/style.css" ) as css:
+    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+    
+
 # Main page
 st.title("AI WorkForce Planning Tools")
 st.write("Welcome to the AI WorkForce Planning Tools! Use the sidebar to navigate to different tools.")
 
-st.sidebar.header("Navigation")
-st.sidebar.write("Choose a page to explore:")
